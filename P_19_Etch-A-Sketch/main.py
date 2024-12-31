045490bb1db57f582ee8753a2b3fcd36cc598d29
RiyaBodke <114491248+RiyaBodke@users.noreply.github.com>
2024-12-31 18:39:57 +0530

    Update main.py

--- P_19_Etch-A-Sketch/main.py
+++ P_19_Etch-A-Sketch/main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
+tim.shape("classic")
 
 def move_forwards():
     tim.forward(10)
@@ -19,6 +20,40 @@ def turn_right():
     
 def clear():
     tim.reset()
+    tim.pensize()
+    
+def small_width():
+    tim.pensize(1)
+
+def medium_width():
+    tim.pensize(5)
+    
+def large_width():
+    tim.pensize(10)
+    
+def color_violet():
+    tim.color("violet")
+    
+def color_indigo():
+    tim.color("indigo")
+    
+def color_teal():
+    tim.color("teal")
+    
+def color_green():
+    tim.color("green")
+    
+def color_yellow():
+    tim.color("yellow")
+    
+def color_orange():
+    tim.color("orange")
+    
+def color_red():
+    tim.color("red")
+    
+def color_black():
+    tim.color("black")
 
 screen.listen()
 
@@ -27,5 +62,16 @@ screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="d", fun=turn_right)
 screen.onkey(key="c", fun=clear)
+screen.onkey(key="b", fun=small_width)
+screen.onkey(key="n", fun=medium_width)
+screen.onkey(key="m", fun=large_width)
+screen.onkey(key="v", fun=color_violet)
+screen.onkey(key="i", fun=color_indigo)
+screen.onkey(key="t", fun=color_teal)
+screen.onkey(key="g", fun=color_green)
+screen.onkey(key="y", fun=color_yellow)
+screen.onkey(key="o", fun=color_orange)
+screen.onkey(key="r", fun=color_red)
+screen.onkey(key="f", fun=color_black)
 
 screen.exitonclick()
