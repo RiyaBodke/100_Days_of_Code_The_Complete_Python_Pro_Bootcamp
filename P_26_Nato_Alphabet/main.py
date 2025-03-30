561fff4244769a4ee6233450d2cc9f93f90788cb
RiyaBodke <114491248+RiyaBodke@users.noreply.github.com>
2025-03-30 16:05:36 +0530

    Update main.py

--- P_26_Nato_Alphabet/main.py
+++ P_26_Nato_Alphabet/main.py
@@ -7,12 +7,14 @@ phonetics = { row.letter: row.code for (index, row) in df.iterrows()}
 # print(phonetics)
 
 # Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter your name : ").upper()
+def generate_pheonetic():
 
-try:
-        output_list = [phonetic_dict[letter] for letter in word]
-except KeyError:
-        print("ERROR! Enter letters only from alphabets please!")
-        generate_pheonetic()
-else:
-        print(output_list)
+        user_input = input("Enter your name : ").upper()
+        
+        try:
+                output_list = [phonetic_dict[letter] for letter in word]
+        except KeyError:
+                print("ERROR! Enter letters only from alphabets please!")
+                generate_pheonetic()
+        else:
+                print(output_list)
